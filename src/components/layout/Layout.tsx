125de76ce9d5fdc3630a5ef8a5074e6ca1b82133
Alex Bal <alexenri12@gmail.com>
2022-08-20 05:26:49 -0300

    viewport fix-bug

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,24 +1,31 @@
 import { Header } from "components/header/Header";
 import { Menu } from "components/menu/Menu";
 import { ReportCard } from "components/report-card/ReportCard";
-import { changeReportCard } from "hooks";
+import { changeBurguer, changeReportCard } from "hooks";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import css from "./index.css";
 
 export function Layout() {
   const { reportCardState } = changeReportCard();
+  const { burguerState } = changeBurguer();
   return (
-    <div className={`${reportCardState ? css.root : ""}`}>
-      <Header />
-      <main
-        style={{ margin: "20px auto", maxWidth: "600px", padding: "0px 20px" }}
-      >
-        <ReportCard />
+    <div className={`${reportCardState ? css.estatic + " " + css.root : ""}`}>
+      <div className={`${burguerState ? css.estatic : ""}`}>
+        <Header />
+        <main
+          style={{
+            margin: "20px auto",
+            maxWidth: "600px",
+            padding: "0px 20px",
+          }}
+        >
+          <ReportCard />
 
-        <Outlet />
-      </main>
-      <Menu />
+          <Outlet />
+        </main>
+        <Menu />
+      </div>
     </div>
   );
 }
